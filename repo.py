09a5a475619845c10bbfa63fbd2e6ca1b29b7c0c
4elik <83275328+Derski4el@users.noreply.github.com>
2025-02-17 18:05:44 +0300

    Update repo.py

--- repo.py
+++ repo.py
@@ -1,11 +1,11 @@
 from datetime import datetime
 from typing import List
-from uuid import UUID
-from sqlalchemy.orm import mapped_column, Mapped
-from sqlalchemy import Date, DateTime, Enum, String, create_engine
+from sqlalchemy.orm import mapped_column, Mapped, declarative_base
+from sqlalchemy import UUID, Date, DateTime, Enum, ForeignKey, String, create_engine
 import enum
+import uuid
 
-class Base(DeprecationWarning): pass
+Base = declarative_base()
 
 class Status(enum.Enum):
     pending = 'в ожидании'
@@ -13,29 +13,56 @@ class Status(enum.Enum):
     completed = 'выполнено'
     in_not_performed = 'не выполнено'
 
-class StatusUser(enum.Enum):
+class permissionWorkerEnum(enum.Enum):
+    in_progress = 'в работе'
+    in_rest = 'отдыхает'
+
+class PermissionEnum(enum.Enum):
     admin = 'админ'
+    manager = 'менеджер'
+    worker = 'работник'
     user = 'пользователь'
 
-class OrderBase(Base):
-    __tablename__ = 'orders'
-
-    id: Mapped[UUID] = mapped_column(primary_key=True)
-    name: Mapped[str] = mapped_column(String(30))
-    #date: Mapped[Date] = mapped_column()
-    #created_date = DateTime(default=datetime.datetime.now())
-    date_start: Mapped[datetime] = mapped_column(DateTime)
-    date_end: Mapped[datetime] = mapped_column(DateTime)
-    equipment: Mapped[str] = mapped_column(String(30))
-    problem_type: Mapped[str] = mapped_column(String(200))
-    description: Mapped[str] = mapped_column(String(200))
-    status: Mapped[Status] = mapped_column(Enum(Status), default=Status.pending)
-    worker: Mapped[str] = mapped_column(String(30))
-    comment: Mapped[list[str]] = mapped_column(String(500))
 
 class ClientBase(Base):
     __tablename__ = 'clients'
 
-    id: Mapped[UUID] = mapped_column(primary_key=True)
+    id: Mapped[UUID] = mapped_column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
     name: Mapped[str] = mapped_column(String(30))
-    status: Mapped[StatusUser] = mapped_column(Enum(StatusUser), default=StatusUser.user)
+    permission: Mapped[PermissionEnum] = mapped_column(Enum(PermissionEnum), default=PermissionEnum.user)
+
+
+class WorkerBase(ClientBase):
+    __tablename__ = 'workers'
+    id: Mapped[UUID] = mapped_column(String(36), ForeignKey("clients.id"), primary_key=True)
+    status_worker: Mapped[PermissionEnum] = mapped_column(Enum(PermissionEnum), default=PermissionEnum.worker)
+    permission: Mapped[permissionWorkerEnum] = mapped_column(Enum(permissionWorkerEnum), default=permissionWorkerEnum.in_rest)
+
+class ManagerBase(ClientBase):
+    __tablename__ = 'managers'
+    id: Mapped[UUID] = mapped_column(String(36), ForeignKey("clients.id"), primary_key=True)
+    permission: Mapped[PermissionEnum] = mapped_column(Enum(PermissionEnum), default=PermissionEnum.manager)
+
+class AdminBase(ClientBase):
+    __tablename__ = 'admins'
+    id: Mapped[UUID] = mapped_column(String(36), ForeignKey("clients.id"), primary_key=True)
+    permission: Mapped[PermissionEnum] = mapped_column(Enum(PermissionEnum), default=PermissionEnum.admin)
+
+class OrderBase(Base):
+    __tablename__ = 'orders'
+
+    id: Mapped[UUID] = mapped_column(String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    name: Mapped[str] = mapped_column(String(30), nullable=False)
+    date_start: Mapped[datetime] = mapped_column(DateTime, default=datetime.now())
+    date_end: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    equipment: Mapped[str] = mapped_column(String(30), nullable=False)
+    problem_type: Mapped[str] = mapped_column(String(200), nullable=False)
+    description: Mapped[str] = mapped_column(String(200), nullable=False)
+    status: Mapped[Status] = mapped_column(Enum(Status), default=Status.pending, nullable=False)
+    worker: Mapped[str] = mapped_column(String(30), default='Не назначен')
+    comment: Mapped[list[str]] = mapped_column(String(500), default=[])
+
+if __name__ == '__main__':
+    engine = create_engine('sqlite:///database.db', echo=True)
+    Base.metadata.create_all(engine)
+    print("База данных успешно создана!")
